ade9387f45cb6f3bf4eff0c3e7a83c875d358723
Farid Mitri <faridmitri@gmail.com>
2020-12-12 19:08:39 +0200

    resolve bugs

--- app/src/main/java/com/am/chatapp/Login.java
+++ app/src/main/java/com/am/chatapp/Login.java
@@ -1,20 +1,44 @@
 package com.am.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-import org.w3c.dom.Text;
+
 
 public class Login extends AppCompatActivity {
 
     private TextInputEditText editTextEmail,editTextPassword;
-    private Button buttonLogin,buttonSignup;
-    private TextView textForgetpassword;
+    private Button buttonSignin,buttonSignup;
+    private TextView textViewForget;
+
+    FirebaseAuth auth;
+    FirebaseUser firebaseUser;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (firebaseUser != null)
+        {
+            Intent intent = new Intent(Login.this,MainActivity.class);
+            startActivity(intent);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +47,67 @@ public class Login extends AppCompatActivity {
 
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextPassword);
-        buttonLogin = findViewById(R.id.buttonLogin);
+        buttonSignin = findViewById(R.id.buttonSignin);
         buttonSignup = findViewById(R.id.buttonSignup);
-        textForgetpassword = findViewById(R.id.textForgetPassword);
+        textViewForget = findViewById(R.id.textViewForget);
+
+        auth = FirebaseAuth.getInstance();
+
+        buttonSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                if (!email.equals("") && !password.equals(""))
+                {
+                    signin(email,password);
+                }
+                else
+                {
+                    Toast.makeText(Login.this, "Please enter an email and password.", Toast.LENGTH_SHORT).show();
+                }
 
 
+            }
+        });
 
+        buttonSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this,Sign_up.class);
+                startActivity(intent);
+            }
+        });
 
+        textViewForget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this,Forget.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void signin(String email,String password)
+    {
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this,new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful())
+                {
+                    Intent intent = new Intent(Login.this,MainActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(Login.this, "Sign in is successful.", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(Login.this, "Sign in is not successful.", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
     }
 }
