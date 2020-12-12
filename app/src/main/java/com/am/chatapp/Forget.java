db115b88f04fd8e3fe76be573788ba8e0f8c1d6e
Farid Mitri <faridmitri@gmail.com>
2020-12-12 21:24:41 +0200

    login and signup

--- app/src/main/java/com/am/chatapp/Forget.java
+++ app/src/main/java/com/am/chatapp/Forget.java
@@ -1,15 +1,22 @@
 package com.am.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Forget extends AppCompatActivity {
     private TextInputEditText editTextForget;
     private Button buttonForget;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +25,37 @@ public class Forget extends AppCompatActivity {
         
         editTextForget = findViewById(R.id.editTextForget);
         buttonForget = findViewById(R.id.buttonForget);
+        auth = FirebaseAuth.getInstance();
+        buttonForget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editTextForget.getText().toString();
+                if (!email.equals(""))
+                {
+                    passwordReset(email);
+                }
+            }
+        });
+        auth = FirebaseAuth.getInstance();
+
+    }
+
+    public void passwordReset(String email)
+    {
+        auth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task)
+            {
+                if (task.isSuccessful())
+                {
+                    Toast.makeText(Forget.this, "Please check your email.", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(Forget.this, "There is a problem.", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 }
